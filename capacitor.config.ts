b7850e843c8bbdd1e7c6946c16877c2e6d344676
Don-Ivan-117 <increiblejake13@gmail.com>
2024-04-18 20:02:42 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Get_Coordenadas',
+  webDir: 'www'
+};
+
+export default config;
